6f59b3d3fdefc17876badf6c58827c184658e37c
Leslie Hoare <iam@lesleh.co.uk>
2016-12-22 12:05:53 +0000

    Use new pattern classes

--- app.js
+++ app.js
@@ -1,20 +1,34 @@
-import Color from "./color";
-import Rectangle from "./rectangle";
+import Point from "./point";
+import Pattern from "./pattern";
 
-let interval;
+let url;
 
-const draw = function () {
+const draw = function (callback) {
     let canvas = document.getElementById("theCanvas");
     let ctx = canvas.getContext("2d");
-    let rect = new Rectangle(0, 0, canvas.width, canvas.height);
+    ctx.imageSmoothingEnabled = true;
+    callback(ctx);
+};
 
-    //interval = setInterval(function () {
-        let newRect = rect.randomSubset();
-        ctx.fillStyle = Color.randomColor().toHexString();
-        ctx.fillRect(newRect.x, newRect.y, newRect.width, newRect.height);
-    //}, 50);
+const linesBetweenPoints = function (ctx, points) {
+    for (let i = 0; i < points.length - 1; ++i) {
+        for (let j = i + 1; j < points.length; ++j) {
+            ctx.beginPath();
+            ctx.moveTo(points[i].x, points[i].y);
+            ctx.lineTo(points[j].x, points[j].y);
+            ctx.stroke();
+        }
+    }
+};
+
+const drawPattern = function (ctx) {
+    let numberOfPoints = parseInt(url.searchParams.get("p")) || 10;
+    let pattern = new Pattern(numberOfPoints, new Point(250, 250), 240);
+    let points = pattern.points();
+    linesBetweenPoints(ctx, points);
 };
 
 document.addEventListener("DOMContentLoaded", function () {
-    draw();
+    url = new URL(document.location);
+    draw(drawPattern);
 });
